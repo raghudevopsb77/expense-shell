972eff400919a91e4addc763bef67cb64237e5f3
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-29 07:42:35 +0530

    Created Shell script for frontend

--- backend.sh
+++ backend.sh
@@ -20,4 +20,4 @@ systemctl enable backend
 systemctl restart backend
 
 dnf install mysql -y
-mysql -h 172.31.25.146 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql-dev.rdevopsb73.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
