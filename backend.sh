7ef93932e48230ac755ded4d4c0d19ac0af02e5e
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-30 08:02:57 +0530

    Shell Code

--- backend.sh
+++ backend.sh
@@ -1,44 +1,50 @@
 MYSQL_PASSWORD=$1
 log_file=/tmp/expense.log
-echo -e "\e[35mDisable Default Version of NodeJS\e[0m"
+
+Head() {
+  echo "\e[35m$1\e[0m"
+}
+
+
+Head "Disable Default Version of NodeJS"
 dnf module disable nodejs -y &>>$log_file
 
-echo -e "\e[35mEnable NodeJS18 Version\e[0m"
+Head "Enable NodeJS18 Version"
 dnf module enable nodejs:18 -y &>>$log_file
 
-echo -e "\e[35mInstall NodeJS\e[0m"
+Head "Install NodeJS"
 dnf install nodejs -y &>>$log_file
 
-echo -e "\e[35mConfigure Backend Service\e[0m"
+Head "Configure Backend Service"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
-echo -e "\e[35mAdding Application User\e[0m"
+Head "Adding Application User"
 useradd expense &>>$log_file
 
-echo -e "\e[35mRemove existing App content\e[0m"
+Head "Remove existing App content"
 rm -rf /app &>>$log_file
 
-echo -e "\e[35mCreate Application Directory\e[0m"
+Head "Create Application Directory"
 mkdir /app &>>$log_file
 
-echo -e "\e[35mDownload Application Content\e[0m"
+Head "Download Application Content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file
 cd /app
 
-echo -e "\e[35mExtracting Application Content\e[0m"
+Head "Extracting Application Content"
 unzip /tmp/backend.zip &>>$log_file
 
-echo -e "\e[35mDownloading Application Dependencies\e[0m"
+Head "Downloading Application Dependencies"
 npm install &>>$log_file
 
-echo -e "\e[35mReloading SystemD and Start Backend Service\e[0m"
+Head "Reloading SystemD and Start Backend Service"
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
 
-echo -e "\e[35mInstall MySQL Client\e[0m"
+Head "Install MySQL Client"
 dnf install mysql -y &>>$log_file
 
-echo -e "\e[35mLoad Schema\e[0m"
+Head "Load Schema"
 mysql -h mysql-dev.rdevopsb73.online -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql &>>$log_file
 
